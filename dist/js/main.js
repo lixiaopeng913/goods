6b977586784349eae5b626069c4c64ae7f0c5225
bjlixiaopeng <bjlixiaopeng@corp.netease.com>
2017-11-11 18:31:14 +0800

    add some event

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,3 +27,14 @@ function getOrderStatus(code) {
         return "未知";
     }
 }
+
+function getURLParams(){
+    var url = location.href;
+    var paraString = url.substring(url.indexOf("?")+1,url.length).split("&");
+    var paraObj = {};
+    var j;
+    for (var i=0; j=paraString[i]; i++){
+        paraObj[j.substring(0,j.indexOf("="))] = j.substring(j.indexOf("=")+1,j.length);
+    }
+    return paraObj;
+}
